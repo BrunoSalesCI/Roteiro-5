87d6a91d9499a6ba224feafbc6f13eb660b03e2e
SobrinhoDeUlisses <30627720+SobrinhoDeUlisses@users.noreply.github.com>
2017-09-16 18:49:46 -0300

    Add files via upload

---
+++ questao6/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "ControleDePagamentos.h"
+#include"Pagamento.h"
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    ControleDePagamentos funcionario1 = ControleDePagamentos();
+
+
+
+    funcionario1.setPagamentos();
+
+    funcionario1.calculaTotalDePagamentos();
+
+    funcionario1.existePagamentoParaFuncionario("antonio");
+
+
+
+
+
+
+
+
+}
